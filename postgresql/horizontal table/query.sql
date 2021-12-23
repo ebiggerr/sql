e8eeb21b3b26dde7bfc06b9f09a30b5f293400ed
Alexious Yong <alexiousyong@outlook.com>
2021-12-23 14:32:48 +0800

    Update query.sql

--- postgresql/horizontal table/query.sql
+++ postgresql/horizontal table/query.sql
@@ -1,5 +1,5 @@
-SELECT MAX(ID) as Max_Id, 
+SELECT MAX(Id) as Max_Id, 
 (
-SELECT Name AS Max_Length_String FROM demo WHERE LENGTH(name) = ( SELECT MAX(LENGTH(NAME)) FROM DEMO )
+SELECT Name AS Max_Length_String FROM demo WHERE LENGTH(name) = ( SELECT MAX(LENGTH(name)) FROM demo )
 ) 
 FROM demo
